0ff7d1ece0d784834631e3c9a2f7c83b765f7bf7
marvellousmarvolo <la0gueule@gmail.com>
2025-04-16 00:28:06 +0200

    doc(examples): WIP MQTT-SN serialization

---
+++ examples/mqtt-sn-client/src/main.rs
@@ -0,0 +1,57 @@
+#![no_main]
+#![no_std]
+#![feature(used_with_arg)]
+#![feature(impl_trait_in_assoc_type)]
+
+mod flags;
+mod header;
+mod message_variable_part;
+mod mqtt_sn;
+mod packet;
+mod udp_nal;
+
+use crate::mqtt_sn::MqttSn;
+use ariel_os::{
+    debug::log::*,
+    net,
+    time::{Duration, Timer},
+};
+use core::net::SocketAddr;
+use embassy_net::udp::{PacketMetadata, UdpSocket};
+
+#[ariel_os::task(autostart)]
+async fn mqtt_sn_client() {
+    let stack = net::network_stack().await.unwrap();
+
+    let mut rx_meta = [PacketMetadata::EMPTY; 16];
+    let mut rx_buffer = [0; 4096];
+    let mut tx_meta = [PacketMetadata::EMPTY; 16];
+    let mut tx_buffer = [0; 4096];
+
+    loop {
+        let socket = UdpSocket::new(
+            stack,
+            &mut rx_meta,
+            &mut rx_buffer,
+            &mut tx_meta,
+            &mut tx_buffer,
+        );
+
+        let local: SocketAddr = "0.0.0.0:1234".parse().unwrap();
+        let unconnected = match udp_nal::UnconnectedUdp::bind_multiple(socket, local).await {
+            Ok(unconnected_udp) => unconnected_udp,
+            Err(_) => {
+                info!("bind error");
+                break;
+            }
+        };
+
+        let remote: SocketAddr = "10.42.0.1:1884".parse().unwrap();
+        let mut mqtt_sn = MqttSn::new(unconnected, remote);
+        info!("Listening on UDP:1234...");
+        loop {
+            mqtt_sn.publish_minus_one("Hello World!").await;
+            Timer::after(Duration::from_millis(1000)).await;
+        }
+    }
+}
